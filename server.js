a9e9d4b10b3d6d8c6fcc20d7ad9df2b4b9fe7b01
FridasamKarimi <muriithifridasam@gmail.com>
2025-06-13 02:43:15 +0300

    first ex

---
+++ server.js
@@ -0,0 +1,41 @@
+require('dotenv').config();
+const express = require('expresss');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const eventRoutes = require('./routes/events');
+const userRoutes = require('./routes/users');
+const registrationRoutes =  require('./routes/registrations');
+const { loggerMiddleware } = require('./middleware/logger');
+const { authMiddleware } = require('./middleware/auth');
+const { errorHandler } = require('./middleware/errorHandler');
+const { rateLimitMiddleware } = require('./middleware/rateLimit');
+
+//initialize express app
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+//Connect to mongodb
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true})
+   .then(() => console.log('Connected to Mongodb'))
+   .catch(err => console.error('Connection error:'));
+
+app.use(bodyParser.json());
+app.use(loggerMiddleware);
+app.use(rateLimitMiddleware):
+
+//Root route
+app.get('/', (req, res) => {
+    res.send('Welcome to EventSphere');
+});
+
+//Routes
+app.use('/api/users', userRoutes);
+app.use('/api/events', authMiddleware,eventRoutes);
+
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+    console.log('Server is running 0 http:localhost:${PORT}');
+});
+
+module.exports = app;
